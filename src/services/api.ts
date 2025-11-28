da3356364c893d67bc056afb806f826a89077470
alexandrehilario <alexandreds969@gmail.com>
2025-11-28 15:34:00 -0300

    .

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Configuração da API
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://dashboard-nordeste-conectado-backend.onrender.com",
   headers: {
     'Content-Type': 'application/json',
   },
