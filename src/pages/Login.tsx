4b14f59ea390e89a616710b4f922a6e163920d59
alexandrehilario <alexandreds969@gmail.com>
2025-11-28 15:42:54 -0300

    .

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,6 +4,7 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { Github } from 'lucide-react';
+import { api } from '../services/api';
 
 const Login = () => {
   const [loginEmail, setLoginEmail] = useState('');
@@ -38,7 +39,7 @@ const Login = () => {
     }
 
     try {
-      const response = await fetch("http://localhost:3333/auth/register", {
+      const response = await api.post("/auth/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
